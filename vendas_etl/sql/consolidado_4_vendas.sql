6fa0c72dc46e14e4e8c801d274879ff6f1da9a88
Tatiane Arruda <tat.arruda1@gmail.com>
2023-05-08 22:10:15 -0300

    estrutura de pastas atualizada

---
+++ vendas_etl/sql/consolidado_4_vendas.sql
@@ -0,0 +1,8 @@
+SELECT
+    v.id_linha, 
+    v.linha,
+    CAST(EXTRACT(MONTH from CAST(v.data_venda AS DATE FORMAT 'DD/MM/YYYY')) AS string)  AS mes,
+    CAST(EXTRACT(YEAR from CAST(v.data_venda AS DATE FORMAT 'DD/MM/YYYY')) AS string)  AS ano,
+    SUM(v.qtd_venda) AS qtd_venda 
+FROM gb_vendas.vendas_totais v 
+GROUP BY v.id_marca, v.marca, v.id_linha, v.linha, v.data_venda
