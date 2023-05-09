dc1f1de3739f47e752a8c1f25e4d743c484b2cdf
Tatiane Arruda <tat.arruda1@gmail.com>
2023-05-08 22:12:33 -0300

    estrutura de pastas atualizada

--- sql/consolidado_2_vendas.sql
+++
@@ -1,8 +0,0 @@
-    SELECT
-        v.id_marca, 
-        v.marca, 
-        v.id_linha, 
-        v.linha, 
-        SUM(v.qtd_venda) AS qtd_venda
-    FROM gb_vendas.vendas_totais v
-    GROUP BY v.id_marca, v.marca, v.id_linha, v.linha
