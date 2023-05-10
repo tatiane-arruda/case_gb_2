89371d572b1b9f0326ba0c2b962cb7a6d77a276a
Tatiane Arruda <tat.arruda1@gmail.com>
2023-05-09 21:31:37 -0300

    created bigquery handler

---
+++ spotify_extract/utils/bigquery_handler.py
@@ -0,0 +1,33 @@
+
+from google.cloud import bigquery
+
+# Create BigQuery client 
+client=bigquery.Client()
+
+def read_data_to_df(query):
+    '''Executes a query and returns a pandas dataframe'''
+    res = client.query(query)
+
+    df = res.to_dataframe()
+    print(df)
+    return df 
+
+
+def load_data_from_df(table_id, df, schema):
+    '''Gets a pandas dataframe and loads the data into BigQuery table'''
+    job_config = bigquery.LoadJobConfig(
+       schema,
+        write_disposition="WRITE_TRUNCATE", # replaces data
+    )
+
+    job = client.load_table_from_dataframe(
+        df, 
+        table_id, 
+        job_config=job_config
+    )
+
+    job.result()   
+
+
+
+
