057d5a9dc8fc4aa21605dae0fa4428e2b515affa
Tatiane Arruda <tat.arruda1@gmail.com>
2023-05-10 10:06:56 -0300

    get espisodes feature created

--- spotify_extract/spotify_extract.py
+++ spotify_extract/spotify_extract.py
@@ -1,62 +1,61 @@
-import os
-import requests
 from google.cloud import bigquery
-import pandas as pd
-from utils.bigquery_handler import client, read_data_to_df, load_data_from_df
-from utils.spotify_api_handler import sp_client, get_podcasts_data
-#from dotenv import load_dotenv
+from utils.bigquery_handler import read_data_to_df, load_data_from_df
+from utils.spotify_api_handler import get_podcasts_data, get_episodes_data
+from dotenv import load_dotenv
 
-#load_dotenv()
+load_dotenv()
 
-################### Get first 50 results of query search "Data Hackers"
-
-# Search params 
-SPOTIFY_URI ="https://api.spotify.com/v1/search"
+# Initialize params  
+SPOTIFY_SEARCH_URI ="https://api.spotify.com/v1/search"
 QUERY = 'Data+Hackers'
 TYPE="show"
 MARKET='ES'
 OFFSET="0"
 LIMIT="50"
 
-podcast_df = get_podcasts_data(SPOTIFY_URI, QUERY, TYPE, MARKET, OFFSET, LIMIT)
+SPOTIFY_SHOW_URI ="https://api.spotify.com/v1/shows"  
+ID="1oMIHOXsrLFENAeM743g93" # Data Hackers https://open.spotify.com/show/1oMIHOXsrLFENAeM743g93?si=eb986ffafa204bd6
 
-################### 
-
-
-
-
-################### Store results in BigQuery
-
-TABLE_PODCASTS_ID='case-gb-2.spotify_data.podcast'
+TABLE_PODCASTS_ID='case-gb-2.spotify_data.podcast' # table_id=f"{PROJECT_ID}.{bg_dataset}.{table_name}"
 TABLE_DATA_HACKERS_GB='case-gb-2.spotify_data.data_hackers_gb'
-TABLE_DATA_HACKERS='case-gb-2.spotify_data.data_hackers_episodes'
+TABLE_DATA_HACKERS='case-gb-2.spotify_data.data_hackers_eps'
 
-#table_id=f"{PROJECT_ID}.{bg_dataset}.{table_name}"
-
-schema_podcasts=[
+SCHEMA_PODCASTS=[
     bigquery.SchemaField("name", "STRING"),
     bigquery.SchemaField("description", "STRING"),
     bigquery.SchemaField("id", "STRING"),
     bigquery.SchemaField("total_episodes", "INTEGER")
 ]
 
-schema_data_hackers=[
-    bigquery.SchemaField("id", "INTEGER"),
+SCHEMA_DATA_HACKERS=[
+    bigquery.SchemaField("id", "STRING"),
     bigquery.SchemaField("name", "STRING"),
     bigquery.SchemaField("description", "STRING"),
-    bigquery.SchemaField("release_date", "DATE"),
-    bigquery.SchemaField("duration_ms", "STRING"),
+    bigquery.SchemaField("release_date", "STRING"),
+    bigquery.SchemaField("duration_ms", "INTEGER"),
     bigquery.SchemaField("language", "STRING"),
-    bigquery.SchemaField("explicit", "STRING"),
-    bigquery.SchemaField("type", "STRING"),
-
+    bigquery.SchemaField("explicit", "BOOLEAN"),
+    bigquery.SchemaField("type", "STRING")
 ]
 
-load_data_from_df(TABLE_PODCASTS_ID, podcast_df, schema_podcasts)
+################### Get first 50 results of query search "Data Hackers"
+
+podcast_df = get_podcasts_data(SPOTIFY_SEARCH_URI, QUERY, TYPE, MARKET, OFFSET, LIMIT)
+
+################### Get Data Hackers Episodes
+
+eps_podcast_df = get_episodes_data(SPOTIFY_SHOW_URI, ID, MARKET, OFFSET,LIMIT)
+
+
+
+load_data_from_df(TABLE_PODCASTS_ID, podcast_df, SCHEMA_PODCASTS)
+# load_data_from_df(TABLE_DATA_HACKERS, eps_podcast_df, SCHEMA_DATA_HACKERS)
+
+
 
 pod_query="""
     SELECT * 
-    FROM spotify_data.podcast
+    FROM spotify_data.data_hackers_eps
     LIMIT 10
 """
 
--- spotify_extract/utils/spotify_api_handler.py
+++ spotify_extract/utils/spotify_api_handler.py
@@ -1,7 +1,6 @@
 import spotify
 import requests
 import base64
-import pprint
 import pandas as pd
 
 cid='73592f8546db4e31ba2ccf2d809c45ad'
@@ -9,25 +8,27 @@ secret='391ff44f0d2749b79b7e344c90fde28a'
 
 sp_client = spotify.Client(cid, secret)
 
-sp_clients_creds=f"{cid}:{secret}"
-sp_clients_creds_b64=base64.b64encode(sp_clients_creds.encode())
+def authenticate():
+    sp_clients_creds=f"{cid}:{secret}"
+    sp_clients_creds_b64=base64.b64encode(sp_clients_creds.encode())
 
-token_url="https://accounts.spotify.com/api/token"
-method="POST"
+    token_url="https://accounts.spotify.com/api/token"
+    #method="POST"
 
-token_data = {
-    "grant_type": "client_credentials"
-}
+    token_data = {
+        "grant_type": "client_credentials"
+    }
 
-token_headers= {
-    "Authorization": f"Basic {sp_clients_creds_b64.decode()}"
-}
+    token_headers= {
+        "Authorization": f"Basic {sp_clients_creds_b64.decode()}"
+    }
 
-req = requests.post(token_url, data=token_data, headers=token_headers)
-token_response_data = req.json()
-access_token=token_response_data["access_token"]
+    req = requests.post(token_url, data=token_data, headers=token_headers)
+    token_response_data = req.json()
+    access_token=token_response_data["access_token"]
+    print(access_token)
 
-print(access_token)
+    return access_token
 
 
 def get_podcasts_data(URI,search, type, market, offset, limit):
@@ -45,13 +46,12 @@ def get_podcasts_data(URI,search, type, market, offset, limit):
     query += f'&offset={offset}'
     query += f'&limit={limit}'
     
+    access_token=authenticate()
     response = requests.get(url=query, headers={
         'Authorization': f'Bearer {access_token}'
     })
 
     json_res = response.json()
-
-#    print(json_res)
 
     for el in range(len(json_res['shows']['items'])):
         name.append(json_res['shows']['items'][el]['name'])
@@ -65,6 +65,60 @@ def get_podcasts_data(URI,search, type, market, offset, limit):
     df['description']=description
     df['id']=id 
     df['total_episodes']=total_episodes
+
+    #print(df)
+
+    return df 
+
+def get_episodes_data(URI,show_id,market, offset, limit):
+    '''Make an API request to Spotify API and returns a pandas dataframe'''
+    
+    id=[]
+    name=[]
+    description=[]
+    release_date=[]
+    duration_ms=[]
+    language=[]
+    explicit=[]
+    type=[]
+
+
+    query = f'{URI}'
+    query += f'/{show_id}/episodes?'
+    query += f'offset={offset}'
+    query +=f'&market={market}'
+    query += f'&limit={limit}'
+
+    access_token=authenticate()
+    response = requests.get(url=query, headers={
+        'Authorization': f'Bearer {access_token}'
+    })
+
+    json_res = response.json()
+
+
+
+    for el in range(len(json_res['items'])):
+        id.append(json_res['items'][el]['id'])
+        name.append(json_res['items'][el]['name'])
+        description.append(json_res['items'][el]['description'])
+        release_date.append(json_res['items'][el]['release_date'])
+        duration_ms.append(json_res['items'][el]['duration_ms'])
+        language.append(json_res['items'][el]['language'])
+        explicit.append(json_res['items'][el]['explicit'])
+        type.append(json_res['items'][el]['type'])
+
+
+    df = pd.DataFrame()
+    
+    df['id']=id
+    df['name'] = name  
+    df['description']=description
+    df['release_date']=release_date
+    df['duration_ms']=duration_ms
+    df['language']=language
+    df['explicit']=explicit 
+    df['type']=type  
 
     print(df)
 
